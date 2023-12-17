a85d24d3b4f6cbf8126f502266ac6d75d43bbbd8
Md Yasir <mdyasir25@gmail.com>
2023-12-17 20:14:05 +0530

    initial commit

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    internal class Calculator
+    {
+        public int CalculatorAdd(int a, int b)
+        {
+            return a+b;
+        }
+        public int CalculatorSubtract(int a, int b)
+        {
+            return a-b;
+        }
+        public int CalculatorMultiply(int a, int b)
+        {
+            return a*b;
+        }
+        public int CalculatorDivide(int a, int b)
+        {
+            return a/b;
+        }
+    }
+}
---
+++ CalculatorLibrary/CalculatorTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private readonly Calculator _calculator;
+        public CalculatorTest()
+        {
+            _calculator = new Calculator();
+        }
+        [TestMethod]
+        public void CalculatorAddTest()
+        {
+            var res = _calculator.CalculatorAdd(5, 4);
+            Assert.AreEqual(9, res);
+        }
+        [TestMethod]
+        public void CalculatorSubtractTest()
+        {
+            var res = _calculator.CalculatorSubtract(5, 4);
+            Assert.AreEqual(1, res);
+        }
+        [TestMethod]
+        public void CalculatorMultiplyTest()
+        {
+            var res = _calculator.CalculatorMultiply(5, 4);
+            Assert.AreEqual(20, res);
+        }
+        [TestMethod]
+        public void CalculatorDivideTest()
+        {
+            var res = _calculator.CalculatorDivide(8, 4);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
